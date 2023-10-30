6cf261fbae926f0393e9f7d812c0e70a6e58827b
drazakd <drazakd@gmail.com>
2023-10-30 11:49:25 +0000

    version 1 suppression de commentaire

--- Zoro/app.py
+++ Zoro/app.py
@@ -1,10 +1,9 @@
 from flask import Flask,render_template, request
 from flask import LoginManager, login_user, login_required, logout_user, UserMixin, current_user
-from flask import Bootstrap
 
 app=Flask(__name__)#montre le nom (app) de notre application a flask
 
-#les routes sont les chemin qui vont nous permettre d afficher notre page elle se font grace a @nomdelapp.route("/") / pour la route
+
 
 @app.route("/") #page principale pour specifier le chemin
 def connexion(): #nom de la fontion
